abe12bfd0b565a6b9cc6e28efe0214f8cf55f669
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-07-07 15:18:05 +0200

    Edit navbar Add error page

--- frontend/src/pages/Nav/Nav.jsx
+++ frontend/src/pages/Nav/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
       <a href="/" className="flex items-center">
           <img src={logo} className="h-8 mr-3" alt="Flowbite Logo" />
       </a>
-      <div className="flex items-center md:order-2">
+      <div className="flex text-center md:order-2">
           <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
             <span className="sr-only">Open user menu</span>
             <img className="w-8 h-8 rounded-full" src={user_icon} alt="user photo" />

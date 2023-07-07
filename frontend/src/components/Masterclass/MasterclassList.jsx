896114575d6bb534a0662933c6f9776b8c6d9732
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2023-07-07 11:24:03 +0200

    Add scearch in masterclasslist

--- frontend/src/components/Masterclass/MasterclassList.jsx
+++ frontend/src/components/Masterclass/MasterclassList.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 import useGetMasterclassList from "../../Hook/useGetMasterclassList";
 
-const MasterclassList = () => {
+const MasterclassList = (props) => {
   const [masterclassList, setMasterclassList] = useState([]);
   const getMasterclassList = useGetMasterclassList();
 
@@ -13,24 +13,29 @@ const MasterclassList = () => {
     });
   }, [])
 
+  const filteredData = masterclassList.filter((el) => {
+    if (props.input === '') { //if no input the return the original
+      return el;
+    } else { //return the item which contains the user input
+      return el.title.toLowerCase().includes(props.input)
+    }
+  })
+  
   return (
-    <>
-      <h1 className='text-3xl my-12 font-medium text-center text-primary_first font-black'>Masterclasses</h1>
-      <div className="flex flex-col my-4 lg:flex md:flex">
-        <div className='lg:grid md:grid lg:grid-cols-3 md:grid-cols-2 flex flex-wrap gap-4'>
-          {
-            masterclassList.map((masterclass, index) => (
-              <NavLink key={index} to={`/masterclass/${masterclass.id}`}
-                          className="rounded-lg grid p-4 w-full gap-y-4 shadow-shadow_2">
-                <h3 className="text-xl font-black">{masterclass.title}</h3>
-                <p className="text-sm font-black text-mid_neutral">{masterclass.description.substring(0, 100)}</p>
-                <p className="text-sm font-black text-right text-primary_first">En Savoir plus</p>
-              </NavLink>
-            ))
-          }
-        </div>     
-      </div>
-    </>
+    <div className="flex flex-col my-4 lg:flex md:flex">
+      <div className='lg:grid md:grid lg:grid-cols-3 md:grid-cols-2 flex flex-wrap gap-4'>
+        {
+          filteredData.map((masterclass, index) => (
+            <NavLink key={index} to={`/masterclass/${masterclass.id}`}
+                        className="rounded-lg grid p-4 w-full gap-y-4 shadow-shadow_2">
+              <h3 className="text-xl font-black">{masterclass.title}</h3>
+              <p className="text-sm font-black text-mid_neutral">{masterclass.description.substring(0, 100)}</p>
+              <p className="text-sm font-black text-right text-primary_first">En Savoir plus</p>
+            </NavLink>
+          ))
+        }
+      </div>     
+    </div>
   );
 };
 

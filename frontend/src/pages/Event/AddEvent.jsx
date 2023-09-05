3803848379619c7c1f71f2d59259e0dc8d9cf478
farah-doghri <farah.doghri@gmail.com>
2023-09-05 15:52:07 +0200

    Update of event pages and components

--- frontend/src/pages/Event/AddEvent.jsx
+++ frontend/src/pages/Event/AddEvent.jsx
@@ -3,8 +3,8 @@ import UseAddEvent from '../../components/Event/AddEvent';
 
 export default function AddEvent() {
     return (
-        <div>
-            <h1 className="text-center">Nouvel événement</h1>
+        <div className='w-full p-12'>
+            <h1 className="text-3xl mt-6 mb-1 font-medium text-primary_first font-black">Nouvel événement</h1>
             <UseAddEvent></UseAddEvent>
         </div>
     );
